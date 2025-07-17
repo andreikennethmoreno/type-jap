2e7e4f841d42c07c6e4b7dbb250296684ba028f4
andreikennethmoreno <124364969+andreikennethmoreno@users.noreply.github.com>
2025-07-17 08:47:27 +0800

    fix: Type error: Type '{ params: { converter: string; }; }' does not satisfy the constraint 'PageProps

--- src/app/(root)/convert/[converter]/page.tsx
+++ src/app/(root)/convert/[converter]/page.tsx
@@ -2,11 +2,13 @@ import TextConverter from "@/components/text-converter";
 import { converters } from "@/lib/converters";
 import { notFound } from "next/navigation";
 
-export default function ConverterPage({
-  params,
-}: {
-  params: { converter: string };
-}) {
+interface PageProps {
+  params: {
+    converter: string;
+  };
+}
+
+export default async function ConverterPage({ params }: PageProps) {
   const converter = converters[params.converter];
 
   if (!converter) return notFound();
