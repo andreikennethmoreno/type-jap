2de443c646a1fa6a88fe5a0954f3b459bddbaccd
andreikennethmoreno <124364969+andreikennethmoreno@users.noreply.github.com>
2025-07-17 19:28:05 +0800

    feat: skeleton

--- src/app/(root)/convert/[converter]/page.tsx
+++ src/app/(root)/convert/[converter]/page.tsx
@@ -3,6 +3,8 @@
 import TextConverter from "@/components/text-converter";
 import { notFound } from "next/navigation";
 import type { TextConverterDefinition } from "@/interface/converter.interface";
+import { Suspense } from "react";
+import SkeletonLoader from "@/components/skeleton-loader";
 
 export default async function ConverterPage({
   params,
@@ -13,8 +15,12 @@ export default async function ConverterPage({
 
   let converterConfig: TextConverterDefinition | null = null;
 
+  //const wait = (ms: number) => new Promise((res) => setTimeout(res, ms));
+
+
   try {
     // Dynamically import the converter file using the slug
+    //await wait(2000);
     const mod = await import(`@/lib/converters/${converter}`);
     converterConfig = mod.default;
   } catch (err) {
@@ -25,10 +31,12 @@ export default async function ConverterPage({
   if (!converterConfig) return notFound();
 
   return (
-    <TextConverter
-      slug={converter}
-      name={converterConfig.name}
-      description={converterConfig.description}
-    />
+    <Suspense fallback={<SkeletonLoader />}>
+      <TextConverter
+        slug={converter}
+        name={converterConfig.name}
+        description={converterConfig.description}
+      />
+    </Suspense>
   );
 }
