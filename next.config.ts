61ec02d4f8604da39e9b08178625bcc5cfe8c6ec
andreikennethmoreno <124364969+andreikennethmoreno@users.noreply.github.com>
2025-07-22 21:05:16 +0800

    feat: prisma init

--- next.config.ts
+++ next.config.ts
@@ -1,9 +1,14 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
   images: {
-    domains: ["img.clerk.com"],
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "img.clerk.com",
+        pathname: "/**", // allow all paths under the domain
+      },
+    ],
   },
 };
 
