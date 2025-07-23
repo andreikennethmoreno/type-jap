07e2ea7b69452fe9248284086cc55ab8e9ffcf6f
andreikennethmoreno <124364969+andreikennethmoreno@users.noreply.github.com>
2025-07-23 22:57:59 +0800

    fix: use async promise for params

--- src/app/(root)/trainer/[script]/[mode]/page.tsx
+++ src/app/(root)/trainer/[script]/[mode]/page.tsx
@@ -2,12 +2,10 @@ import { notFound } from "next/navigation";
 import Trainer from "@/components/trainer";
 import { JSX } from "react";
 
-export default async function Page({
-  params,
-}: {
+export default async function Page(props: {
   params: { script: string; mode: string };
 }): Promise<JSX.Element> {
-  const { script, mode } = params;
+  const { script, mode } = await props.params; 
 
   return <Trainer script={script} mode={mode} />;
 }
